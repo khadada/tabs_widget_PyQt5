2b979f04e8426e0755e6c2a58b631702373f5c74
khadada <lkhadada@gmail.com>
2022-04-19 18:30:34 +0100

    All done with initialize_ui method

--- main.py
+++ main.py
@@ -2,3 +2,16 @@
 import sys
 from PyQt5.QtWidgets import(QApplication,QMainWindow,QWidget,QTabWidget)
 from custom_widget_placeholder import Color
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initialize_ui()
+        
+    def initialize_ui(self):
+        """
+        Initalize the main window then display its content
+        """
+        self.setWindowTitle("Tab_")
+        self.setGeometry(40, 40, 600, 800)
+        
